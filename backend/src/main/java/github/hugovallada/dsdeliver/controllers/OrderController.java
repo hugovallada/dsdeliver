35424c6fad2cf9529219c9b3e1df6abc21d6e5c8
Hugo Vallada <valladahugo@gmail.com>
2021-01-04 14:42:32 -0300

    Create post endpoint for orders

--- backend/src/main/java/github/hugovallada/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/github/hugovallada/dsdeliver/controllers/OrderController.java
@@ -4,10 +4,10 @@ import github.hugovallada.dsdeliver.dtos.OrderDto;
 import github.hugovallada.dsdeliver.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,5 +21,13 @@ public class OrderController {
     public ResponseEntity<List<OrderDto>> getAll(){
         var orders = service.findAll();
         return ResponseEntity.ok().body(orders);
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderDto> insert(@RequestBody OrderDto orderDto){
+        var savedOrderDto = service.insert(orderDto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(savedOrderDto.getId()).toUri();
+        return ResponseEntity.created(uri).body(savedOrderDto);
     }
 }
