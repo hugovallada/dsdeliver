ca44165923306ba7ec89ef4c930d0e018bd61292
Hugo Vallada <valladahugo@gmail.com>
2021-01-06 12:02:23 -0300

    Add Main Image

---
+++ front-web/src/Home/index.tsx
@@ -0,0 +1,29 @@
+import {ReactComponent as MainImage} from './main.svg';
+import './styles.css';
+
+function Home(){
+    return(
+        <div className="home-container">
+            <div className="home-content">
+                <div className="home=action">
+                    <h1 className="home-title">
+                        Faça seu pedido <br/> que entregamos <br/> pra você!!!
+                    </h1>
+                    <h3 className="home-subtitle">
+                    Escolha o seu pedido e em poucos minutos <br/> levaremoss na sua porta
+                    </h3>
+                    <a href="orders" className="home-btn-order">
+                        FAZER PEDIDO
+                    </a>
+                </div>
+                <div className="home-image">
+                    <MainImage />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+//TODO: Parado no 1:25:30
+
+export default Home;
