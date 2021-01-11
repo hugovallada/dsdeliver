f43a936f72ca54348c54a0a0e7a146b4c71a8152
Hugo Vallada <valladahugo@gmail.com>
2021-01-10 23:04:54 -0300

    Finish front end

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
 import Routes from './Routes';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div>
       <>
         <Routes />
+        <ToastContainer />
       </>
     </div>
   );
---
+++ front-web/src/Orders/OrderSummary.tsx
@@ -0,0 +1,32 @@
+import { formatPrice } from "./helpers";
+
+type Props = {
+    amount: number;
+    totalPrice: number;
+    onSubmit:() => void;
+}
+
+function OrderSummary({amount, totalPrice, onSubmit}: Props) {
+  return (
+    <div className="order-summary-container">
+      <div className="order-summary-content">
+        <div>
+          <span className="amount-selected-container">
+            <strong className="amount-selected">{amount}</strong>
+            {amount !== 1 ? 'PEDIDOS SELECIONADOS' : 'PEDIDO SELECIONADO'}
+          </span>
+        
+          <span className="order-summary-total">
+            <strong className="amount-selected">{formatPrice(totalPrice)}</strong>
+            VALOR TOTAL
+          </span>
+        </div>
+        <button className="order-summary-make-order" onSubmit={onSubmit}>
+            FAZER PEDIDO
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default OrderSummary;
